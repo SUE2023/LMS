192e3021746b53047c2298cceba026a473b19cf0
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2024-10-21 12:39:57 +0000

    Home Route: Enable static Home page to make it possible to have information about Power Session Library

--- app.py
+++ app.py
@@ -56,7 +56,8 @@ def index():
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+   # return render_template('Home.html')
+   return send_from_directory('static', 'Home.html')
 
 @app.route('/dashboard')
 @login_required
